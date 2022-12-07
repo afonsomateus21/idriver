4c96d2236343bd75f242e7a43b869cbdb83ac2be
afonsomateus21 <mateusdeoliveira1111@gmail.com>
2022-12-07 02:07:06 -0300

    layout formulario de cadastro

--- src/pages/UserRegister/index.tsx
+++ src/pages/UserRegister/index.tsx
@@ -1,5 +1,79 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { SideBar } from "../../components/SideBar";
+import { Container, Content } from "./styles";
+
+type InputsType = {
+  cnh: string;
+  name: string;
+  address: string;
+  email: string;
+  phone: string;
+  licensePlate: string;
+  model: string;
+  year: string;
+  carMaker: string;
+}
+
 export function UserRegister() {
+  const { register, handleSubmit } = useForm<InputsType>();
+
+  const onSubmit: SubmitHandler<InputsType> = data => console.log(data);
+
   return (
-    <h1>User Register</h1>
+    <>
+      <SideBar />
+      <Container>
+        <Content onSubmit={handleSubmit(onSubmit)}>
+          <h1>Cadastrar Motorista</h1>
+
+          <div>
+            <label htmlFor="cnh">Número da CNH</label>
+            <input placeholder="Digite o número da sua CNH" {...register("cnh")} />
+          </div>
+
+          <div>
+            <label htmlFor="name">Nome</label>
+            <input placeholder="Digite seu nome" {...register("name")} />
+          </div>
+          
+          <div>
+            <label htmlFor="address">Endereço</label>
+            <input placeholder="Digite o seu endereço" {...register("address")} />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email</label>
+            <input placeholder="Digite o seu email" type="email" {...register("email")} />
+          </div>
+
+          <div>
+            <label htmlFor="phone">Número do telefone</label>
+            <input placeholder="Digite seu telefone" {...register("phone")} />
+          </div>
+
+          <div>
+            <label htmlFor="licensePlate">Placa do Carro</label>
+            <input placeholder="Digite o número da sua placa" {...register("licensePlate")} />
+          </div>
+
+          <div>
+            <label htmlFor="model">Modelo do Carro</label>
+            <input placeholder="Digite o modelo do seu carro" {...register("model")} />
+          </div>
+
+          <div>
+            <label htmlFor="year">Ano do Carro</label>
+            <input placeholder="Digite o ano do seu carro" {...register("year")} />
+          </div>
+
+          <div>
+            <label htmlFor="carMaker">Fabricante</label>
+            <input placeholder="Digite o fabricante do seu carro" {...register("carMaker")} />
+          </div>
+
+          <button type="submit">Enviar</button>
+        </Content>
+      </Container>
+    </>
   );
 }
--- src/pages/UserRegister/styles.ts
+++ src/pages/UserRegister/styles.ts
@@ -1,3 +1,54 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.main`
+  height: 100%;
+  position: fixed;
+  left: 13rem;
+  width: 90%;
+`
+
+export const Content = styled.form`
+  max-width: 600px;
+  margin: 0 auto;
+  height: 100%;
+
+  h1 {
+    font-size: 1.7rem;
+    text-align: center;
+    padding-top: 0.5rem ;
+  }
+
+  input {
+    display: block;
+    box-sizing: border-box;
+    width: 100%;
+    border-radius: 4px;
+    border: 1px solid black;
+    padding: 10px 15px;
+    margin-bottom: 10px;
+    font-size: 14px;
+  }
+
+  label {
+    text-align: left;
+    display: block;
+    margin-top: 1rem;
+    margin-bottom: 0.5rem;
+    color: var(--black);
+    font-size: 14px;
+    font-weight: 200;
+  }
+
+  button {
+    background: var(--black);
+    color: white;
+    text-transform: uppercase;
+    border: none;
+    /* margin-top: 40px; */
+    padding: 1rem;
+    font-size: 1rem;
+    font-weight: 100;
+    letter-spacing: 10px;
+    width: 100%;
+  }
+`
